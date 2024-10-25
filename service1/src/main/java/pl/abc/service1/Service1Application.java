76ebe7bab5d2147c04770d2fa25cb353eeead525
Bartoszbockowski <bartoszbockowskisoft@gmail.com>
2024-10-25 11:19:10 +0200

    update: remove unnecessary properties file for kafka connection

--- service1/src/main/java/pl/abc/service1/Service1Application.java
+++ service1/src/main/java/pl/abc/service1/Service1Application.java
@@ -2,10 +2,7 @@ package pl.abc.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import pl.abc.service1.properties.KafkaProperties;
 
-@EnableConfigurationProperties(KafkaProperties.class)
 @SpringBootApplication
 public class Service1Application {
 
--- service1/src/test/java/pl/abc/service1/listenter/TestKafkaListener.java
+++ service1/src/test/java/pl/abc/service1/listenter/TestKafkaListener.java
@@ -3,6 +3,7 @@ package pl.abc.service1.listenter;
 import lombok.Getter;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,7 +15,6 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.test.context.ActiveProfiles;
 import pl.abc.service1.model.dto.BookDto;
-import pl.abc.service1.properties.KafkaProperties;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,26 +30,29 @@ public class TestKafkaListener {
     private final List<BookDto> created = new ArrayList<>();
     private final List<BookDto> rented = new ArrayList<>();
 
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>>
-    kafkaListenerContainerFactory(KafkaProperties kafkaProperties) {
+    kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory(kafkaProperties));
+        factory.setConsumerFactory(consumerFactory());
         factory.setConcurrency(1);
         factory.getContainerProperties().setPollTimeout(3000);
         return factory;
     }
 
     @Bean
-    public ConsumerFactory<Integer, String> consumerFactory(KafkaProperties kafkaProperties) {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(kafkaProperties));
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
     @Bean
-    public Map<String, Object> consumerConfigs(KafkaProperties kafkaProperties) {
+    public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "book-service");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BookEventDeserializer.class);
